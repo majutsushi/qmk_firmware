89cfcc45da65ff654d122cd8108ff7bbf2ea1cff
Jan Larres <jan@majutsushi.net>
2017-02-07 18:37:12 +1300

    add majutsushi keymap

---
+++ keyboards/ergodox/keymaps/majutsushi/config.h
@@ -0,0 +1,13 @@
+// https://github.com/jackhumbert/qmk_firmware#the-configh-file
+
+#ifndef CONFIG_USER_H
+#define CONFIG_USER_H
+
+#include "../../config.h"
+
+#define PREVENT_STUCK_MODIFIERS
+
+#undef TAPPING_TERM
+#define TAPPING_TERM    150
+
+#endif
