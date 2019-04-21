b1974bf2acd625e9a9dacc3e916a58da8793d35a
Jan Larres <jan@majutsushi.net>
2019-04-21 19:22:52 +1200

    Use right control on right side

--- keyboards/ergodox_ez/keymaps/majutsushi/keymap.c
+++ keyboards/ergodox_ez/keymaps/majutsushi/keymap.c
@@ -35,7 +35,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         // left hand
         KC_EQL,         KC_1,         KC_2,     KC_3,   KC_4,   KC_5,   TG(SYMB),
         KC_TAB,         KC_Q,         KC_W,     KC_E,   KC_R,   KC_T,   KC_APP,
-        CTL_T(KC_ESC),  KC_A,         KC_S,     KC_D,   KC_F,   KC_G,
+        LCTL_T(KC_ESC), KC_A,         KC_S,     KC_D,   KC_F,   KC_G,
         KC_LSPO,        KC_Z,         KC_X,     KC_C,   KC_V,   KC_B,   KC_GRV,
         KC_LSFT,        KC_QUOT,      MO(SYMB), GUI_T(KC_LBRC), ALT_T(KC_SPC),
                                                KC_DELT,  KC_LGUI,
@@ -44,7 +44,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         // right hand
              TG(NUMK),    KC_6,   KC_7,   KC_8,          KC_9,    KC_0,             KC_MINS,
              KC_APP,      KC_Y,   KC_U,   KC_I,          KC_O,    KC_P,             KC_BSLS,
-                          KC_H,   KC_J,   KC_K,          KC_L,    KC_SCLN,          CTL_T(KC_QUOT),
+                          KC_H,   KC_J,   KC_K,          KC_L,    KC_SCLN,          RCTL_T(KC_QUOT),
              KC_RGUI,     KC_N,   KC_M,   KC_COMM,       KC_DOT,  KC_SLSH,          KC_RSPC,
                                   KC_FN2, GUI_T(KC_RBRC),MO(SYMB),KC_RBRC,          KC_RSFT,
              KC_LALT,        KC_INS,
